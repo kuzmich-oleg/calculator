e574fe2441b7d0536e144372a81aaed4f7866197
kuzmich-oleg <kuzmicholeg1@gmail.com>
2017-09-03 23:23:57 +0300

    Fixing

--- common.js
+++ common.js
@@ -1,19 +1,28 @@
-var result;
+var result = "";
 var temp_string = "";
 var optional_box = document.getElementsByClassName("optional-box")[0];
 var result_screen = document.getElementsByClassName("result")[0];
+
+function throw_err() {
+	optional_box.style.backgroundColor = "red";
+	setTimeout(function () {optional_box.style.backgroundColor = "white"}, 1000);
+	result = "";
+}
 
 function calculate() {
 	"use strict";
 	try {
 		result = eval(temp_string);
+		if (result=== Infinity) {
+			throw_err();
+		} 
 	} catch (err) {
-		result = "try again";
+		throw_err();
 	}
 
 	if (result) {
-		result_screen.innerHTML = result;
-	}
+			result_screen.innerHTML = result;
+	} 
 	temp_string = result;
 }
 
